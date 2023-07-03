4883c2690bef7ca9052c15b64ed5eee16dde11ed
bwithchewoo <notmissleo@Leo-Desktop.localdomain>
2023-07-02 20:46:09 -0400

    finished work experience section

--- src/Work.js
+++ src/Work.js
@@ -20,16 +20,17 @@ const Card = ({ title, content, company, dates, additionalContent }) => {
           <p>{company}</p>
           <p>{dates}</p>
           <p>{content}</p>
+          
         </div>
         <div className="back">
-          <p>{additionalContent}</p> 
-        </div>
+            <p>{additionalContent}</p> 
+            </div>
       </div>
     );
     };
 
 const Carousel = ({ children }) => {
-  const [active, setActive] = useState(2);
+  const [active, setActive] = useState(0);
   const count = React.Children.count(children);
 
   return (
@@ -71,35 +72,73 @@ const Work = () => {
           company: 'Altman, Greenfield & Selvaggi',
           dates: 'October 2021 to July 2022',
           content: 'New York, NY',
-          additionalContent: "its me"
+          additionalContent: (
+            <div>
+            <p>Performed day-to-day full charge bookkeeping functions for clients.</p>
+            <p>Input information provided by clients in the form of invoices, bills, checks, or other registers.</p>
+            <p>Performed check runs utilizing different software packages, mostly Datafaction with Imaging.</p>
+            <p>Prepared journal entries, bank reconciliations, and payroll.</p>
+            <p>Received and inputted clients' accounts payable invoices and generated payment.</p>
+            <p>Prepared appropriate schedules and reports as requested by clients and partners.</p>
+            <p>Created accounting systems and set up bank accounts, credit cards, and loans for new clients.</p>
+          </div>
+          )
         },
         {
-          title: 'Card 2',
-          company: 'Company 2',
-          dates: '2019-2020',
+          title: 'Accounting Manager',
+          company: 'Jethmalani & Nallaseth PLLC',
+          dates: 'March 2020 to October 2021',
           content: 'New York, NY',
-          additionalContent: "its me"
+          additionalContent: (<div>
+            <p>Handled receivables and payables in the business, making sure that our firm gets paid for the work that we did by following up with clients regarding payments and sending them statements.</p>
+            <p>Reconciled credit card statements and bank statements.</p>
+            <p>Ensured that the firm has supplies that it needs such as checks, envelopes and other necessities even through Covid-19.</p>
+            <p>Worked with the India Team in regards to evaluation invoices and India Office expenses.</p>
+            <p>Onboarded new hires to the firm, giving them any information that they need and helping them acclimate to the new work environment.</p>
+            <p>Processed payroll every two weeks and kept track of sick/vacation day usage by employees.</p>
+            <p>Generate checks for the USCIS for our client's applications.</p>
+            <p>During the lockdown, worked with our employees and employers to ensure a smooth transition to working remotely by having meetings and receiving daily reports. Made sure that our employees had access to necessary tools to keep working such as remote computer access, Box, email, Fedex and Slack.</p>
+          </div>)
         },
         {
-          title: 'Card 3',
-          company: 'Company 3',
-          dates: '2018-2019',
+          title: 'Bookkeeper',
+          company: 'Brighton Management',
+          dates: 'February 2019 to March 2020',
           content: 'New York, NY',
-          additionalContent: "its me"
+          additionalContent: (<div>
+            <p>Generated financial statements and facilitated account closing procedures each month for each property in the business.</p>
+            <p>Processed rent payments and applied to tenant balances.</p>
+            <p>Maintained integrity of general ledger, including chart of accounts.</p>
+            <p>Reconciled 26 accounts for each property in our portfolio.</p>
+            <p>Entered invoices from vendors and ensured that bills are paid.</p>
+            <p>Provided extensive clerical and administrative support to department staff.</p>
+          </div>)
         },
         {
-          title: 'Card 4',
-          company: 'Company 4',
-          dates: '2017-2018',
+          title: 'Accounting Assistant',
+          company: 'U.S. Jewelry House',
+          dates: 'March 2017 - March 2020',
           content: 'New York, NY',
-          additionalContent: "its me"
+          additionalContent: (<div>
+            <p>Created an excel sheet to input all the daily sales data from various stores that U.S. Jewelry House operates.</p>
+            <p>Updated Quickbooks with the cash, credit card and check receipts of all sales.</p>
+            <p>Organized a daily report regarding all the expenses and sales information for the Vice President.</p>
+          </div>)
         },
         {
-          title: 'Card 5',
-          company: 'Company 5',
-          dates: '2016-2017',
+          title: 'Accounting Assistant',
+          company: 'Sarah Laird Inc',
+          dates: 'March 2018 - January 2019',
           content: 'New York, NY',
-          additionalContent: "its me"
+          additionalContent: (<div>
+            <p>Compared budgeted costs to actual costs while invoicing to ensure that artists and photographers are being paid for everything their jobs entail.</p>
+            <p>Generated invoices from paperwork submitted by artists and photographers and ensured there are no discrepancies in their calculations.</p>
+            <p>Followed up on outstanding invoices to get payment updates.</p>
+            <p>Inputted callsheets into Salesforce and related them to our artists and photographers.</p>
+            <p>Used Photoshop when fixing invoices or creating a markup based on estimated costs.</p>
+            <p>Prepared accurate sales tax, cash receipt reports, and accounts receivable at the end of every month.</p>
+            <p>Created a breakdown of payment for artists and photographers, deducting their contracted agency fee and paying for their expenses.</p>
+          </div>)
         },
       ];
     
