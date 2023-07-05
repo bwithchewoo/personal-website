dd3d4c8e0c4a412a961e26427f638edac3ceb742
Anna Cho <loveablessing@gmail.com>
2023-07-05 15:53:03 -0400

    added links to icons

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contact from './Contact';
 import { Linkedin } from 'react-feather'
 import { GitHub } from 'react-feather'
 import photo from './Photo.jpg'
+import medium from './medium.png'
 function App() {
   return (
     <Router>
@@ -19,22 +20,29 @@ function App() {
             <img className="image" src={photo} style={{ width: 270, height: 320 }} />
             <p>Anna Cho</p>
             <div className="icons-container">
-              <Linkedin className="icon" />
-              <GitHub className="icon" />
+              <a href="https://www.linkedin.com/in/anna-cho-a30038167/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit" }}>
+                <Linkedin className="icon" />
+              </a>
+              <a href="https://github.com/bwithchewoo" target="_blank" rel="noopener noreferrer" style={{ color: "inherit" }}>
+                <GitHub className="icon" />
+              </a>
+              <a href="https://medium.com/@loveablessing" target="_blank" rel="noopener noreferrer" >
+                <img src={medium} style={{ marginLeft: 5, width: 40, height: 40 }} />
+              </a>
             </div>
           </div>
-          
+
         </div>
         <div className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/work" element={<Work />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </div>
-    </Router>
+    </Router >
   );
 }
 
